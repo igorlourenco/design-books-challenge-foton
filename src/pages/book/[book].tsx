d908a83b6a87ff8eccb0d8c423ab47df9035b60d
Igor Lourenço <theigorlourenco@gmail.com>
2021-02-12 08:42:50 -0300

    feat: added 'add as favorite' functionality

--- src/pages/book/[book].tsx
+++ src/pages/book/[book].tsx
@@ -9,8 +9,9 @@ const Book = () => {
   const router = useRouter()
   const { book } = router.query
   const [likes, setLikes] = useState(158)
-  const [myBook, setMyBook] = useState()
+  const [myBook, setMyBook] = useState(null)
   const [isLiked, setIsLiked] = useState(false)
+  const [isFavorite, setIsFavorite] = useState(false)
 
   useEffect(() => {
     fetch(`https://www.googleapis.com/books/v1/volumes/${book}`).then(async (response) => {
@@ -25,63 +26,65 @@ const Book = () => {
     setIsLiked(!isLiked)
   }
 
-  if (!myBook) {
-    return (<Loading/>)
+  const handleUpdateAddAsFavorite = () => {
+    setIsFavorite(!isFavorite)
   }
 
-  if (myBook) {
-    console.log(myBook)
-    return (
-      <Stack>
-        <Stack padding={[4, 4, 12, 12]} backgroundColor={'yellow.personal'}>
-          <Stack>
-            <Stack isInline spacing={8} justifyContent={'center'} width={'100%'}>
-              <Image src={myBook?.volumeInfo?.imageLinks.thumbnail} height="200px" width="170px" shadow={'2xl'}/>
-              <Stack spacing={2}>
-                <Text fontWeight={'bold'} fontSize={'lg'}>{myBook?.volumeInfo?.title}</Text>
-                <Text fontSize={'sm'} color={'gray.500'}>by {myBook?.volumeInfo?.authors.map(author => author)}</Text>
-                <Stack spacing={2} isInline>
-                  <Text>R$ {myBook?.saleInfo?.retailPrice.amount}</Text>
+  return (
+    myBook
+      ? (
+        <Stack>
+          <Stack padding={[4, 4, 12, 12]} backgroundColor={'yellow.personal'}>
+            <Stack>
+              <Stack isInline spacing={8} justifyContent={'center'} width={'100%'}>
+                <Image src={myBook.volumeInfo?.imageLinks.thumbnail} height="auto" width="auto" shadow={'2xl'}/>
+                <Stack spacing={2}>
+                  <Text fontWeight={'bold'} fontSize={'lg'}>{myBook.volumeInfo?.title}</Text>
+                  <Text fontSize={'sm'} color={'gray.500'}>by {myBook.volumeInfo?.authors.map(author => author)}</Text>
                   <Stack spacing={2} isInline>
-                    <Box as={AiFillStar}/>
-                    <Box as={AiFillStar}/>
-                    <Box as={AiFillStar}/>
-                    <Box as={AiFillStar}/>
-                    <Box as={AiFillStar}/>
+                    <Text> {myBook?.saleInfo?.retailPrice ? `R$ ${myBook.saleInfo.retailPrice.amount}` : 'price not informed'}</Text>
+                    <Stack spacing={2} isInline>
+                      <Box as={AiFillStar}/>
+                      <Box as={AiFillStar}/>
+                      <Box as={AiFillStar}/>
+                      <Box as={AiFillStar}/>
+                      <Box as={AiFillStar}/>
+                    </Stack>
                   </Stack>
+                  <Divider/>
+                  <Text fontSize={'xs'} fontWeight={'bold'} color={'gray.700'}>{likes} likes</Text>
+                  <Text fontSize={'xs'} color={'gray.700'}>{isFavorite ? 'Marcado como favorito' : ''}</Text>
                 </Stack>
-                <Divider/>
-                <Text fontSize={'xs'} fontWeight={'bold'} color={'gray.700'}>{likes} likes</Text>
-                <Text fontSize={'xs'} color={'gray.700'}>Marcado como favorito</Text>
               </Stack>
-            </Stack>
-            <Stack
-              spacing={2}
-              paddingX={[0, 0, 10, 10]}
-              isInline
-              alignItems="center"
-              width={'100%'}
-              justifyContent={{ base: 'space-between' }}
-            >
-              <Text fontWeight={'bold'}>{myBook?.volumeInfo?.pageCount} pages</Text>
-              <Stack spacing={2} isInline>
-                <Button variant="solid" size="md" colorScheme={'blue'} borderRadius={'25px'} paddingX={4}>
-                  Like
-                </Button>
-                <IconButton variant={isLiked ? 'solid' : 'outline'} aria-label="icon" borderRadius={'50%'} colorScheme={'pink'}
-                            onClick={handleUpdateLike} icon={<Box as={BiHeart}/>} size="md"/>
+              <Stack
+                spacing={2}
+                paddingX={[0, 0, 10, 10]}
+                isInline
+                alignItems="center"
+                width={'100%'}
+                justifyContent={{ base: 'space-between' }}
+              >
+                <Text fontWeight={'bold'}>{myBook?.volumeInfo?.pageCount} pages</Text>
+                <Stack spacing={2} isInline>
+                  <Button variant={isLiked ? 'solid' : 'outline'} size="md" colorScheme={'blue'} borderRadius={'25px'} paddingX={4} onClick={handleUpdateLike}>
+                    {isLiked ? 'Liked' : 'Like'}
+                  </Button>
+                  <IconButton variant={isFavorite ? 'solid' : 'outline'} aria-label="icon" borderRadius={'50%'}
+                              colorScheme={'pink'}
+                              onClick={handleUpdateAddAsFavorite} icon={<Box as={BiHeart}/>} size="md"/>
+                </Stack>
               </Stack>
             </Stack>
           </Stack>
+          <Stack width={'100%'} padding={3} alignItems={'center'}>
+            <Text textAlign={'justify'} width={['95vw', '80vw', '70vw', '70vw']}>
+              {myBook?.volumeInfo?.description}
+            </Text>
+          </Stack>
         </Stack>
-        <Stack width={'100%'} padding={3} alignItems={'center'}>
-          <Text textAlign={'justify'} width={['95vw', '80vw', '70vw', '70vw']}>
-            {myBook?.volumeInfo?.description}
-          </Text>
-        </Stack>
-      </Stack>
-    )
-  }
+        )
+      : (<Loading/>)
+  )
 }
 
 export default Book
