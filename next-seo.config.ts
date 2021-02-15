fa2e191c5b2d5f0554ea206dd93b300a19c5b074
Igor Lourenço <theigorlourenco@gmail.com>
2021-02-15 16:40:04 -0300

    feat: added SEO settings (title, description and favicon)

---
+++ next-seo.config.ts
@@ -0,0 +1,17 @@
+const title = 'Design Books'
+const description = 'Find, save and buy books on the biggest database ever!'
+
+const SEO = {
+  title,
+  description,
+  canonical: 'https://design-books.vercel.app',
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://design-books.vercel.app',
+    title,
+    description
+  }
+}
+
+export default SEO
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../style/theme'
 import { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
